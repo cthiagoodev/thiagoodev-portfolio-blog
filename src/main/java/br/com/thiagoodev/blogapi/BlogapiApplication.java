334438380ba1c03efae1877866a3fb61180ca28d
thiagoodev <cthiagoodev@gmail.com>
2025-07-24 12:31:08 -0300

    app config

--- src/main/java/br/com/thiagoodev/blogapi/BlogapiApplication.java
+++ src/main/java/br/com/thiagoodev/blogapi/BlogapiApplication.java
@@ -1,9 +1,11 @@
 package br.com.thiagoodev.blogapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SpringBootConfiguration
 public class BlogapiApplication {
 
 	public static void main(String[] args) {
