ff9818f5e6df226dc1f5ed5e2aa197beeebc44eb
thiagoodev <cthiagoodev@gmail.com>
2025-07-30 00:30:47 -0300

    correcoes

--- src/main/java/br/com/thiagoodev/blogapi/application/usecases/GetUserByUuidUseCase.java
+++ src/main/java/br/com/thiagoodev/blogapi/application/usecases/GetUserByUuidUseCase.java
@@ -2,7 +2,9 @@ package br.com.thiagoodev.blogapi.application.usecases;
 
 import br.com.thiagoodev.blogapi.domain.entities.User;
 import br.com.thiagoodev.blogapi.domain.services.UserService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GetUserByUuidUseCase {
     private final UserService userService;
 
