313e53dbaa8cfd1452de32923a4511b80f6423ae
thiagoodev <cthiagoodev@gmail.com>
2025-09-14 22:50:03 -0300

    jwt token config

---
+++ src/main/java/br/com/thiagoodev/blogapi/infrastructure/config/JwtConfig.java
@@ -0,0 +1,14 @@
+package br.com.thiagoodev.blogapi.infrastructure.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class JwtConfig {
+    @Value("${jwt.issuer}")
+    private String issuer;
+    @Value("${jwt.secret}")
+    private String secret;
+}
