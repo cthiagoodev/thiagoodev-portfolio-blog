01406bea4f01aa8f3480c2dd9582c9ee1c4970f5
thiagoodev <cthiagoodev@gmail.com>
2025-09-17 00:10:22 -0300

    create jwt filter

--- src/main/java/br/com/thiagoodev/blogapi/infrastructure/config/SecurityConfig.java
+++ src/main/java/br/com/thiagoodev/blogapi/infrastructure/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package br.com.thiagoodev.blogapi.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    static private final String[] PUBLIC_MATCHERS = {
+    public static final String[] PUBLIC_MATCHERS = {
         "/api/auth/login",
         "/api/users/register",
     };
@@ -31,5 +33,11 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
