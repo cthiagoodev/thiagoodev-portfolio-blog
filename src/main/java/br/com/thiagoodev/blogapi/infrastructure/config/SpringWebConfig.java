d5da1c3ce6603e3c3bfd95013317c9d1cd57e90b
thiagoodev <cthiagoodev@gmail.com>
2025-09-23 22:46:05 -0300

    spring web config

--- src/main/java/br/com/thiagoodev/blogapi/infrastructure/config/SpringWebConfig.java
+++ src/main/java/br/com/thiagoodev/blogapi/infrastructure/config/SpringWebConfig.java
@@ -1,22 +1,29 @@
 package br.com.thiagoodev.blogapi.infrastructure.config;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
-public class TemplateConfig {
-    private final ApplicationContext applicationContext;
+@EnableWebMvc
+@ComponentScan
+public class SpringWebConfig implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
 
-    public TemplateConfig(ApplicationContext applicationContext) {
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
 
     @Bean
-    public SpringResourceTemplateResolver templateResolver() {
+    public SpringResourceTemplateResolver templateResolver(){
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(this.applicationContext);
         templateResolver.setPrefix("/WEB-INF/templates/");
@@ -27,10 +34,17 @@ public class TemplateConfig {
     }
 
     @Bean
-    public SpringTemplateEngine templateEngine() {
+    public SpringTemplateEngine templateEngine(){
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
     }
 }
