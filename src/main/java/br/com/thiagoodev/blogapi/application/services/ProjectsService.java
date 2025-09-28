5b2f816ce550f5f20d09777b991d9152a7d43ddf
thiagoodev <cthiagoodev@gmail.com>
2025-09-28 14:00:30 -0300

    config projects service

---
+++ src/main/java/br/com/thiagoodev/blogapi/application/services/ProjectsService.java
@@ -0,0 +1,11 @@
+package br.com.thiagoodev.blogapi.application.services;
+
+import br.com.thiagoodev.blogapi.infrastructure.persistence.repositories.ProjectsRepository;
+
+public class ProjectsService {
+    private final ProjectsRepository repository;
+
+    public ProjectsService(ProjectsRepository repository) {
+        this.repository = repository;
+    }
+}
