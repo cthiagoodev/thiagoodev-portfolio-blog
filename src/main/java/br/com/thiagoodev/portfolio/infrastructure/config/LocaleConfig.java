d997eab4f27907072eed6f476764f0b1638a8ae3
thiagoodev <cthiagoodev@gmail.com>
2025-12-21 20:09:09 -0300

    alteracoes

---
+++ src/main/java/br/com/thiagoodev/portfolio/infrastructure/config/LocaleConfig.java
@@ -0,0 +1,35 @@
+package br.com.thiagoodev.portfolio.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.time.Duration;
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver("LANGUAGE");
+        resolver.setDefaultLocale(Locale.of("pt", "BR"));
+        resolver.setCookieMaxAge(Duration.ofDays(7));
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
